ec7e90f9fe55e0deec511f3a7aa8244e4ea9185c
daigor <daigor.rodrigues@gmail.com>
2022-03-30 10:42:47 +0100

    dr:feature branch

--- src/main/java/com/qa/ims/persistence/domain/Items.java
+++ src/main/java/com/qa/ims/persistence/domain/Items.java
@@ -2,4 +2,85 @@ package com.qa.ims.persistence.domain;
 
 public class Items {
 
+	private Long id;
+	private String Name;
+	private String description;
+
+	public Items (String Name, String description) {
+		this.setFirstName(Name);
+		this.setSurname(description);
+	}
+
+	public Items(Long id, String Name, String description) {
+		this.setId(id);
+		this.setFirstName(Name);
+		this.setSurname(description);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return Name;
+	}
+
+	public void setFirstName(String Name) {
+		this.Name = Name;
+	}
+
+	public String getSurname() {
+		return description;
+	}
+
+	public void setSurname(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "id:" + id + " first name:" + Name + " description:" + description;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Name == null) ? 0 : Name.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Items other = (Items) obj;
+		if (getFirstName() == null) {
+			if (other.getFirstName() != null)
+				return false;
+		} else if (!getFirstName().equals(other.getFirstName()))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		return true;
+	}
+
 }
--- src/main/java/com/qa/ims/persistence/domain/Orders.java
+++ src/main/java/com/qa/ims/persistence/domain/Orders.java
@@ -2,4 +2,85 @@ package com.qa.ims.persistence.domain;
 
 public class Orders {
 
+	private Long orderNumber;
+	private String itemName;
+	private String description;
+
+	public Orders(String itemName, String description) {
+		this.setItemName(itemName);
+		this.setDescription(description);
+	}
+
+	public Orders(Long orderNumber, String itemName, String description) {
+		this.setOrderNumber(orderNumber);
+		this.setItemName(itemName);
+		this.setDescription(description);
+	}
+
+	public Long getOrderNumber() {
+		return orderNumber;
+	}
+
+	public void setOrderNumber(Long orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "orderNumber:" + orderNumber + " Item:" + itemName + " description:" + description;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((itemName == null) ? 0 : itemName.hashCode());
+		result = prime * result + ((orderNumber == null) ? 0 : orderNumber.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Orders other = (Orders) obj;
+		if (getItemName() == null) {
+			if (other.getItemName() != null)
+				return false;
+		} else if (!getItemName().equals(other.getItemName()))
+			return false;
+		if (orderNumber == null) {
+			if (other.orderNumber != null)
+				return false;
+		} else if (!orderNumber.equals(other.orderNumber))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		return true;
+	}
+
 }
